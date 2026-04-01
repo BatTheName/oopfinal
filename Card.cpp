9e15a3dc7311977d5a06eb994177c5918283e5dd
BatTheName <barrennames@gmail.com>
2026-04-01 09:55:35 +0700

    Add files via upload

---
+++ Card.cpp
@@ -0,0 +1,33 @@
+#include "Card.h"
+
+Card::Card()
+{
+	cardNumber = 0;
+	cardType = "NULL";
+}
+
+Card::Card(int number, string type)
+{
+	cardNumber = number;
+	cardType = type;
+}
+
+void Card::setCardNumber(int newNumber)
+{
+	cardNumber = newNumber;
+}
+
+void Card::setCardType(string newType)
+{
+	cardType = newType;
+}
+
+int Card::getCardNumber()
+{
+
+}
+
+string Card::getCardType()
+{
+
+}
---
+++ Card.h
@@ -0,0 +1,28 @@
+#ifndef CARD_H
+#define CARD_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Card
+{
+private:
+	int cardNumber;
+	string cardType;
+
+public:
+	Card();
+	Card(int cardNumber, string cardType);
+
+	void setCardNumber(int newNumber);
+	void setCardType(string newType);
+
+	int getCardNumber();
+	string getCardType();
+};
+
+
+
+#endif
